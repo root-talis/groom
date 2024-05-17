4d532490673d80340a7f038715b61d4f5de57765
George K. <root.talis@gmail.com>
2024-05-17 17:14:11 +0300

    tidy up #[DTO] macro

--- groom_macros/src/dto.rs
+++ groom_macros/src/dto.rs
@@ -5,7 +5,7 @@ use proc_macro2::TokenStream;
 use syn::parse2;
 use quote::{quote};
 
-// region: DTO args ----------------------------------------------------------------
+// region: DTO args --------------------------------------------------------------------------------
 //
 
 #[derive(FromMeta)]
@@ -18,31 +18,28 @@ pub(crate) struct DtoArgs {
 }
 
 //
-// endregion: DTO args -------------------------------------------------------------
+// endregion: DTO args -----------------------------------------------------------------------------
 
-// region: AST parsing and generation ----------------------------------------------
+// region: AST parsing and generation --------------------------------------------------------------
 //
 
 pub(crate) fn generate(args_t: TokenStream, args: DtoArgs, input: TokenStream) -> TokenStream {
-    let generated_impl = generate_impl(args_t, args, input);
-
-    quote! {
-        #generated_impl
-    }
-}
-
-fn generate_impl(args_t: TokenStream, args: DtoArgs, input: TokenStream) -> TokenStream {
     match parse2::<Item>(input) {
-        Err(error) => error.to_compile_error(),
+        Err(error) =>
+            error.to_compile_error(),
+
         Ok(item) => match item {
-            Item::Struct(item_struct) => generate_impl_for_struct(args_t, args, item_struct),
-            _ => Error::new_spanned(item, "DTO should be a struct.").to_compile_error(),
+            Item::Struct(item_struct) =>
+                generate_impl_for_struct(args_t, args, item_struct),
+
+            _ =>
+                Error::new_spanned(item, "DTO should be a struct.").to_compile_error(),
         }
     }
 }
 
 fn generate_impl_for_struct(_args_t: TokenStream, args: DtoArgs, item_struct: ItemStruct) -> TokenStream {
-    let ident = item_struct.ident.clone();
+    let ident = &item_struct.ident;
 
     let (deserialize_derive, dto_request_impl) =
         if !args.request {
@@ -78,4 +75,4 @@ fn generate_impl_for_struct(_args_t: TokenStream, args: DtoArgs, item_struct: It
 }
 
 //
-// endregion: AST parsing and generation ---------------------------------------------------
+// endregion: AST parsing and generation -----------------------------------------------------------
