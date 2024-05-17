9599e2e476e1205d4a4d2dea58ec0c76fce97e1d
George K. <root.talis@gmail.com>
2024-05-17 13:47:26 +0300

    move macro scratchpad out of the way

--- groom_tests/tests/integration/simple_api.rs
+++ groom_tests/tests/integration/simple_api.rs
@@ -3,22 +3,14 @@ use axum::{Extension, Router};
 use axum::body::Body;
 use axum::http::{HeaderMap, Request, StatusCode};
 
-use utoipa::openapi::path::{OperationBuilder, ParameterBuilder, PathItemBuilder};
-use utoipa::openapi::request_body::RequestBodyBuilder;
-use utoipa::openapi::{self, ComponentsBuilder, ContentBuilder, PathsBuilder};
-use utoipa::ToSchema;
 use utoipa::{OpenApi, openapi::OpenApiBuilder};
-
 
 use tower::ServiceExt; // for `call`, `oneshot` and `ready`
 use http_body_util::BodyExt;
 
 use crate::groom_macros::Controller;
 
-// for scratchpad: ------
-use crate::groom::extract::GroomExtractor;
-use crate::integration::simple_api::my_api::{MyState, RqConsPathStruct};
-// end for scratchpad ---
+use crate::integration::simple_api::my_api::MyState;
 
 use serde_json::json;
 
@@ -487,82 +479,6 @@ mod my_api {
 
 // region: tests --------------------------------------------------
 //
-
-#[tokio::test]
-async fn api_doc_scratchpad() {
-    #[derive(OpenApi)]
-    #[openapi()]
-    struct ApiDoc;
-
-    let api = OpenApiBuilder::from(ApiDoc::openapi());
-
-    // region: should generate this for /hello-world
-    let parameter = ParameterBuilder::new()
-        .parameter_in(openapi::path::ParameterIn::Query)
-        .name("sad")
-        .schema(Some(my_api::RqConsQueryParams::schema().1))
-        .build()
-    ;
-
-    let component_schema_name = RqConsPathStruct::schema().0;
-
-    let components = ComponentsBuilder::new()
-        .schema(component_schema_name, my_api::RqConsPathStruct::schema().1)
-        .build()
-    ;
-
-    let content = ContentBuilder::new()
-        .schema(components.schemas[component_schema_name].clone())
-        .build()
-    ;
-
-    let request_body = RequestBodyBuilder::new()
-        .content("application/json", content)
-        .build()
-    ;
-
-    let op_builder= OperationBuilder::new()
-        .parameter(parameter)
-        .request_body(Some(request_body))
-        .description(Some("description"))
-        .summary(Some("summary"));
-
-    let op_builder = axum::extract::Path::<RqConsPathStruct>::__openapi_modify_operation(op_builder);
-
-    let resp = utoipa::openapi::ResponseBuilder::new().build();
-
-    let op_builder = op_builder.response("202", resp);
-
-    let operation = op_builder.build();
-
-    let operation = OperationBuilder::from(operation).build(); 
-
-    let path_item= PathItemBuilder::new()
-        .operation(utoipa::openapi::PathItemType::Get, operation)
-        .build()
-    ;
-
-    let paths = PathsBuilder::new()
-        .path("/hello-world", path_item)
-        .build()
-    ;
-    let paths = PathsBuilder::from(paths).build();
-
-    let api = api.paths(paths);
-    let api = api.components(Some(components));
-
-    // endregion: should generate this for /hello-world
-
-    let json = api.build().to_pretty_json().expect("expected a valid json");
-
-    let _breakpoint = false;
-
-    let hm = ::axum::http::header::HeaderMap::new();
-    let _has_header = hm.get(::axum::http::header::ACCEPT);
-
-    eprintln!("{}", json);
-    //assert_eq!(true, false)
-}
 
 /// In this test we check how OpenAPI spec is generated.
 #[test]
---
+++ groom_tests/tests/scratchpad.rs
@@ -0,0 +1,92 @@
+/// This is a scratchpad to test out how various code pieces fit together
+/// so that we can then turn them into code generation.
+///
+/// Nothing important here, really. This is a mess and it should be so.
+
+use utoipa::{OpenApi, openapi, ToSchema};
+use utoipa::openapi::{ComponentsBuilder, ContentBuilder, OpenApiBuilder, PathsBuilder};
+use utoipa::openapi::path::{OperationBuilder, ParameterBuilder, PathItemBuilder};
+use utoipa::openapi::request_body::RequestBodyBuilder;
+use groom_macros::DTO;
+use groom::extract::GroomExtractor;
+
+#[DTO(request, response)]
+struct SomeDTO {
+    name: String
+}
+
+#[tokio::test]
+async fn api_doc_scratchpad() {
+    #[derive(OpenApi)]
+    #[openapi()]
+    struct ApiDoc;
+
+    let api = OpenApiBuilder::from(ApiDoc::openapi());
+
+    // region: should generate this for /hello-world
+    let parameter = ParameterBuilder::new()
+        .parameter_in(openapi::path::ParameterIn::Query)
+        .name("sad")
+        .schema(Some(SomeDTO::schema().1))
+        .build()
+    ;
+
+    let component_schema_name = SomeDTO::schema().0;
+
+    let components = ComponentsBuilder::new()
+        .schema(component_schema_name, SomeDTO::schema().1)
+        .build()
+        ;
+
+    let content = ContentBuilder::new()
+        .schema(components.schemas[component_schema_name].clone())
+        .build()
+        ;
+
+    let request_body = RequestBodyBuilder::new()
+        .content("application/json", content)
+        .build()
+        ;
+
+    let op_builder= OperationBuilder::new()
+        .parameter(parameter)
+        .request_body(Some(request_body))
+        .description(Some("description"))
+        .summary(Some("summary"));
+
+    let op_builder = axum::extract::Path::<SomeDTO>::__openapi_modify_operation(op_builder);
+
+    let resp = utoipa::openapi::ResponseBuilder::new().build();
+
+    let op_builder = op_builder.response("202", resp);
+
+    let operation = op_builder.build();
+
+    let operation = OperationBuilder::from(operation).build();
+
+    let path_item= PathItemBuilder::new()
+        .operation(utoipa::openapi::PathItemType::Get, operation)
+        .build()
+        ;
+
+    let paths = PathsBuilder::new()
+        .path("/hello-world", path_item)
+        .build()
+        ;
+    let paths = PathsBuilder::from(paths).build();
+
+    let api = api.paths(paths);
+    let api = api.components(Some(components));
+
+    // endregion: should generate this for /hello-world
+
+    let json = api.build().to_pretty_json().expect("expected a valid json");
+
+    let _breakpoint = false;
+
+    let hm = ::axum::http::header::HeaderMap::new();
+    let _has_header = hm.get(::axum::http::header::ACCEPT);
+
+    eprintln!("{}", json);
+    //assert_eq!(true, false)
+}
