9e5c701b3b4314106cd37ea62803388011c3a349
George K. <root.talis@gmail.com>
2026-06-10 13:38:18 +0300

    examples/todo: remove playwright from frontend project

--- examples/todo/frontend/eslint.config.ts
+++ examples/todo/frontend/eslint.config.ts
@@ -1,7 +1,6 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
-import pluginPlaywright from 'eslint-plugin-playwright'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
 
@@ -20,11 +19,6 @@ export default defineConfigWithVueTs(
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-
-  {
-    ...pluginPlaywright.configs['flat/recommended'],
-    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-  },
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
